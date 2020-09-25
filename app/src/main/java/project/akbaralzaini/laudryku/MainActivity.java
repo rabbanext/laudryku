2e350497b56d06a8673548bc924eccbec73dcf34
akbaralzaini <akbaralzaini@gmail.com>
2020-09-25 08:27:26 +0700

    login register

--- app/src/main/java/project/akbaralzaini/laudryku/MainActivity.java
+++ app/src/main/java/project/akbaralzaini/laudryku/MainActivity.java
@@ -8,18 +8,28 @@ import android.widget.Button;
 
 public class MainActivity extends Activity {
     Button btnLogin;
+    Button btnRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         btnLogin = findViewById(R.id.login_button);
+        btnRegister = findViewById(R.id.btn_register);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent login = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(login);
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent register = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(register);
             }
         });
     }
